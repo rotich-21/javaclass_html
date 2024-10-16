4e88e641942b7b7b6dc780405c219c7c620ef5a8
rotich-21 <koechhiram01@gmail.com>
2024-10-16 15:35:31 +0300

    Initial commit

---
+++ assets/Js/my.js
@@ -0,0 +1,10 @@
+
+//get the button element
+const javaclass=document.getElementById('colorButton');
+
+//add event listener for the button click
+
+javaclass.addEventListener('click', ()=>{
+//change the color of the button
+    javaclass.style.backgroundColor=javaclass.style.backgroundColor==='blue' ? '4CAF50':'blue'
+})
